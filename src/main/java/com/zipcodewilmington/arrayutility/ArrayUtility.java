70fdb5688770bacdf11921941258493892051bc2
Devi <diptikadevi@gmail.com>
2024-08-02 11:30:30 -0400

    finished!!

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -4,6 +4,7 @@ package com.zipcodewilmington.arrayutility;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -12,18 +13,28 @@ import java.util.stream.Stream;
  */
 public class ArrayUtility<T> {
 
-    T[] arr;
+    List<T> arr = new ArrayList<>();
+    T[]input;
 
     public ArrayUtility(T[] inputArray) {
-//        ArrayList<T> newArr = (ArrayList<T>) Arrays.asList(inputArray);
-//
-//        ArrayList<T> al =
-//                new ArrayList<T>(Arrays.asList(inputArray));
-        arr = inputArray;
+        input = inputArray;
+
+//        List<T> newArr = new ArrayList<>();
+//        for (T element : inputArray){
+//            newArr.add(element);
+//        }
+//        arr = newArr;
+
+        //NOTE: this is a concise version of above
+        arr = new ArrayList<>(Arrays.asList(inputArray));
+
+
     }
 
-    public Integer countDuplicatesInMerge(T arr, T valueToEvaluate) {
-    return null;
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        Collections.addAll(arr, arrayToMerge);
+
+        return getNumberOfOccurrences(valueToEvaluate);
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
@@ -39,27 +50,36 @@ public class ArrayUtility<T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-//        T[] newCopy = Arrays.copyOf(arrayToMerge, arrayToMerge.length + arr.length);
-//        ArrayList<T> newnewCopy = new ArrayList<>();
-//        @SuppressWarnings("unchecked")
-//        T[] both = Stream.concat(Arrays.stream(arrayToMerge), Arrays.stream(arr)).toArray(
-//                size -> (T[]) Array.newInstance(arr.getClass().getComponentType(), size));
 
-        // ????????????????????????
+        Collections.addAll(arr, arrayToMerge);
+
         T common = null;
         int count = 0;
         int maxCount = 0;
-            for (int i = 0; i < arrayToMerge.length; i++){
-                for (int j =0; j < arrayToMerge.length; j++){
-                    if (arrayToMerge[i].equals(arrayToMerge[j])){
+            for (int i = 0; i < arr.size(); i++){
+                for (int j =0; j < arr.size(); j++){
+                    if (arr.get(i).equals(arr.get(j))){
                         count++;
                     }
                     if (count > maxCount){
-                        common = arrayToMerge[i];
+                        common = arr.get(i);
+                        maxCount = count;
+                    }
+                    if (j == arr.size()-1){
+                        count =0;
                     }
                 }
             }
         return common;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        for (int i = 0; i< arr.size(); i++){
+            if (arr.get(i) == valueToRemove){
+                arr.remove(i);
+            }
         }
 
+        return this.arr.toArray(Arrays.copyOf(input, arr.size()));
+    }
 }
